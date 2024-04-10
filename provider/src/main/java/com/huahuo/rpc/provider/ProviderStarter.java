0196eed93ed21b91816047a6cc96df87bd16585a
chenjianhui <2294198058@qq.com>
2024-04-10 15:57:35 +0800

    Version 1.0 1.RPC远端调用实现 2.注册中心实现 - 2.1.服务注册 - 2.2.服务发现 - 2.3 负载均衡（随机） - 2.4 心跳检测

--- provider/src/main/java/com/huahuo/rpc/provider/ProviderStarter.java
+++ provider/src/main/java/com/huahuo/rpc/provider/ProviderStarter.java
@@ -12,34 +12,39 @@ import com.huahuo.rpc.server.impl.VertxHttpServer;
 import com.huahuo.rpc.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 
-
+// 服务提供者启动类
 @Slf4j
 public class ProviderStarter {
-    public static void main(String[] args) {
-        try {
-            RpcApplication.init();
-            String serviceName = UserService.class.getName();
-            LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
-            RpcConfig rpcConfig = RpcApplication.getRpcConfig();
-            RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
-            Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
-            ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
-            serviceMetaInfo.setServiceName(serviceName);
-            serviceMetaInfo.setServiceAddress(rpcConfig.getServerHost() + ":" + rpcConfig.getServerPort());
-            try {
-                registry.register(serviceMetaInfo);
-                System.out.println("成功注册服务： " + serviceName + " 到注册中心");
-                System.out.println("服务地址：" + serviceMetaInfo.getServiceAddress());
-            } catch (Exception e) {
-                e.printStackTrace();
-                log.error("register service error", e);
-            }
-            //启动Web服务
-            HttpServer httpServer = new VertxHttpServer();
-            httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
-        } catch (Exception e) {
-            e.printStackTrace();
-            log.error("provider start error", e);
-        }
+  public static void main(String[] args) {
+    try {
+      // 初始化RPC应用
+      RpcApplication.init();
+      String serviceName = UserService.class.getName();
+      // 在本地注册中心中注册服务
+      LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+      RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+      RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+      Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+      ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+      serviceMetaInfo.setServiceName(serviceName);
+      serviceMetaInfo.setServiceAddress(rpcConfig.getServerHost() + ":" + rpcConfig.getServerPort());
+      try {
+        // 向注册中心注册服务信息
+        registry.register(serviceMetaInfo);
+        System.out.println("成功注册服务： " + serviceName + " 到注册中心");
+        System.out.println("服务地址：" + serviceMetaInfo.getServiceAddress());
+      } catch (Exception e) {
+        // 注册服务出错时记录日志
+        e.printStackTrace();
+        log.error("register service error", e);
+      }
+      // 启动Web服务
+      HttpServer httpServer = new VertxHttpServer();
+      httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
+    } catch (Exception e) {
+      // 服务提供者启动出错时记录日志
+      e.printStackTrace();
+      log.error("provider start error", e);
     }
+  }
 }
